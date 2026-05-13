485686e507b2f086af821f936f40e8d0ff945a0e
Frxiizl <Farestomoh@gmail.com>
2026-05-13 08:08:06 +0300

    Initial commit

---
+++ contact.js
@@ -0,0 +1,8 @@
+export const contactConfig = {
+  discordUserId: null,
+  links: [
+    { label: "Discord", value: "expatior" },
+    { label: "Email", value: "Frxiizl@gmail.com" },
+    { label: "TikTok", value: "Frxiizl" },
+  ],
+};
---
+++ donate.js
@@ -0,0 +1,7 @@
+export const donateConfig = {
+  BTC: "bc1q895l4gjsz305kxk9etwvmqjsu7ftsnzwjypcsk",
+  ETH: "0x901C304D247ABeFD6A632801f5FdC6A2c1286192",
+  SOL: "4qCoASLvjve5bHiHxbTpdwAdEsQrHW2UCm1MYFWYSDm9",
+  LTC: "LQMvYBDqBTxXsikJxCVVoDXpK1bDamc1SW",
+  XRP: "rL2Zw1h2WrKV5Gtwo8yeDA7aWs39vtJmoX",
+};
